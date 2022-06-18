1552480edb3a62712dfb942c87e18a21b0aec581
wicho1996 <63920120+wicho1996@users.noreply.github.com>
2022-06-17 21:29:01 -0500

    first commit

---
+++ app/Controllers/Login.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class Login extends Controller
+{
+    public function index(){
+        return 'Hola Mundo';
+    }
+
+    public function getPaginas(){
+      $Mlogin = model(Mlogin::class);
+      $paginas = $Mlogin->getArbolPaginas();
+      
+      echo json_encode($paginas);
+    }
+
+    public function validarUsuario(){
+      $Mlogin = model(Mlogin::class);
+      $data = file_get_contents('php://input');
+      print_r($data);
+      // $users = $Mlogin->getUserByUserName($data);
+      echo json_encode(["estatus" => 1, "mensaje" => 'Mensaje acomodado', "data" => ["users" => $users]]);
+    }
+
+    public function crearUsuario(){
+      $Mlogin = model(Mlogin::class);
+      $data = file_get_contents('php://input');
+      echo json_encode(["estatus" => 1, "mensaje" => 'Mensaje acomodado', "data" => ["users" => $users]]);
+    }
+}
---
+++ app/Models/Mlogin.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+
+class Mlogin extends Model
+{
+    public function getUserByUserName($userName){
+      return $this->db->query("SELECT * FROM usuario WHERE usuario = '$userName'")->getResult();
+    }
+    private function getPaginas(){
+      return $this->db->query("SELECT UP.id AS idPagina, UP.nombre AS nombrePagina, UP.idPagina AS subPagina, UPP.id AS idPermisoPagina, UPP.nombre AS nombrePermiso
+        FROM UPagina UP LEFT JOIN UPermisoPagina UPP ON UPP.idPagina = UP.id
+        WHERE UP.estatus = 1 ORDER BY UP.idPagina DESC, UP.orden
+      ");
+    }
+
+    public function getArbolPaginas(){
+      $paginas = $this->getPaginas()->getResult();
+
+      $resPaginas = [];
+      $permisos = [];
+      foreach ($paginas as $key => $pagina) {
+
+        $pagina->idPermisoPagina&&$permisos[$pagina->idPagina][$pagina->idPermisoPagina] = [
+          "idPermisoPagina" => $pagina->idPermisoPagina,
+          "nombrePermiso" => $pagina->nombrePermiso,
+        ];
+
+        $resPaginas[$pagina->idPagina] = [
+          "idPagina" => $pagina->idPagina,
+          "subPagina" => $pagina->subPagina,
+          "nombrePagina" => $pagina->nombrePagina,
+          "permisos" => isset($permisos[$pagina->idPagina]) ? array_values($permisos[$pagina->idPagina]) : [],
+        ];
+
+      }
+
+      return array_values($resPaginas);
+
+    }
+
+}
