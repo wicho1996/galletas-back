87a93f9f390e3b13d547f4e4ab0bc468f54a3ce9
wicho1996 <63920120+wicho1996@users.noreply.github.com>
2022-08-14 10:07:45 -0500

    Termino de Dispositivo

--- app/Controllers/Dispositivo.php
+++ app/Controllers/Dispositivo.php
@@ -14,7 +14,39 @@ class Dispositivo extends Controller
       $dispositivos = $Mdispositivo->getDispositivos()->getResult();
       echo json_encode($dispositivos);
     }
+
+    public function getDispositivo(){
+      $Mdispositivo = model(Mdispositivo::class);
+      $data = json_decode(file_get_contents('php://input'));
+      $dispositivo = $Mdispositivo->getDispositivo($data->id_movil)->getResult();
+      echo json_encode($dispositivo);
+    }
+
+    public function addDispositivo(){
+      $Mdispositivo = model(Mdispositivo::class);
+      $data = json_decode(file_get_contents('php://input'));
+      $dispositivo = $Mdispositivo->addDispositivo($data);
+      $dispositivo['dispositivos'] = $Mdispositivo->getDispositivos()->getResult();
+      echo json_encode($dispositivo);
+    }
+
+    public function setDispositivo(){
+      $Mdispositivo = model(Mdispositivo::class);
+      $data = json_decode(file_get_contents('php://input'));
+      $dispositivo = $Mdispositivo->setDispositivo($data->id_movil, $data);
+      $dispositivo['dispositivos'] = $Mdispositivo->getDispositivos()->getResult();
+      echo json_encode($dispositivo);
+    }
+
+    public function delDispositivo(){
+      $Mdispositivo = model(Mdispositivo::class);
+      $data = json_decode(file_get_contents('php://input'));
+      $dispositivo = $Mdispositivo->delDispositivo($data->id_movil);
+      $dispositivo['dispositivos'] = $Mdispositivo->getDispositivos()->getResult();
+      echo json_encode($dispositivo);
+    }
  
+    // Mobil
     public function updateUbication(){
       $Mdispositivo = model(Mdispositivo::class);
       $data = json_decode(file_get_contents('php://input'));
--- app/Models/Mdispositivo.php
+++ app/Models/Mdispositivo.php
@@ -10,7 +10,51 @@ class Mdispositivo extends Model
       return $this->db->query("SELECT * FROM  movil;");
     }
 
+    function getDispositivo($idMovil){
+      return $this->db->query("SELECT * FROM movil WHERE id_movil = ?;", [$idMovil]);
+    }
 
+    function addDispositivo($data){
+      $data = [
+        "imei" => isset($data->imei) ? $data->imei : NULL,
+        "celular" => isset($data->celular) ? $data->celular : NULL,
+        "descripcion" => isset($data->descripcion) ? $data->descripcion : NULL,
+        "estatus" => isset($data->estatus) ? $data->estatus : 1,
+        "fecha_creacion" => date("Y-m-d H:m:s"),
+      ];
+
+      $builder = $this->db->table('movil');
+
+      if (!$builder->insert($data)) return ["estatus" => -1, "mensaje" => 'Error al registrar Dispositivo'];
+
+      return ["estatus" => 1, "mensaje" => 'OK'];
+    }
+
+    function setDispositivo($id_movil, $data){
+      $data = [
+        "imei" => isset($data->imei) ? $data->imei : NULL,
+        "celular" => isset($data->celular) ? $data->celular : NULL,
+        "descripcion" => isset($data->descripcion) ? $data->descripcion : NULL,
+        "estatus" => isset($data->estatus) ? $data->estatus : 1,
+        "fecha_creacion" => date("Y-m-d H:m:s"),
+      ];
+      
+      $builder = $this->db->table('movil');
+      $builder->where('id_movil', $id_movil);
+
+      if (!$builder->update($data)) return ["estatus" => -1, "mensaje" => 'Error al actualizar el Dispositivo'];
+      return ["estatus" => 1, "mensaje" => 'OK'];
+    }
+
+    function delDispositivo($id_movil){
+      $builder = $this->db->table('movil');
+      $builder->where('id_movil', $id_movil);
+
+      if (!$builder->delete()) return ["estatus" => -1, "mensaje" => 'Error al borrar el Dispositivo'];
+      return ["estatus" => 1, "mensaje" => 'OK'];
+    }
+
+    // Movil
     public function updateUbication( $latitud, $longitud, $id){
       $res = $this->db->query("UPDATE movil SET lat = ? , lon = ? WHERE id_movil = ?", [$latitud, $longitud, $id]);
       return $res;
